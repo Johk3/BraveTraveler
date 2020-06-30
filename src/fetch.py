4a768038f025a7d33ac2478f57886ec4938c61e3
Johannes Leppäkorpi <user3610@protonmail.com>
2020-07-01 00:39:50 +0300

    init
    Signed-off-by: Johannes Leppäkorpi <user3610@protonmail.com>

---
+++ src/fetch.py
@@ -0,0 +1,8 @@
+class Fetch:
+    """INSERT TEXT"""
+    def __init__(self, data):
+        self.data = data
+
+    def parse(self):
+        """Parse the data to work with frontend"""
+        pass
