74cb94998fbc5eb7fccb8b2361f823a6ed7f64fe
Johannes Leppäkorpi <user3610@protonmail.com>
2020-07-01 14:13:48 +0300

    Added sanic functionality

--- bravetraveler.py
+++ bravetraveler.py
@@ -1,13 +1,21 @@
 from src.prepare import Prepare
-import sanic
+from sanic import Sanic
+from sanic import response
+from sanic.response import json
+from sanic.views import HTTPMethodView
+from sanic_cors import CORS, cross_origin
 
-class BraveTraveler:
+app = Sanic(__name__)
+CORS(app)
+
+class BraveTraveler(HTTPMethodView):
     """Monitoring service for incoming data"""
-    def fetchData(self):
-        return 1
+    async def get(self, request):
+        return 202
+    async def post(self, request):
+        Prepare(request.json)
+        return 202
 
+app.add_route(BraveTraveler.as_view(), "/api/bravetraveler")
 if __name__ == "__main__":
-    BraveTraveler = BraveTraveler()
-    data = BraveTraveler.fetchData()
-
-    Fetch = Prepare(data)
+    app.run(host="0.0.0.0", port=8080, access_log=True, workers=5)
--- src/prepare.py
+++ src/prepare.py
@@ -1,8 +1,11 @@
+from log import *
+
 class Prepare:
     """INSERT TEXT"""
     def __init__(self, data):
         self.data = data
+        self.parse()
 
     def parse(self):
         """Parse the data to work with frontend"""
-        pass
+        LOG_INFO(self.data)
