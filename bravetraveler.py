4ccf47880f8781b5193e72fa8c078945cf76178f
Johannes Leppäkorpi <user3610@protonmail.com>
2020-07-03 00:55:51 +0300

    Implemented Authentication

--- bravetraveler.py
+++ bravetraveler.py
@@ -4,23 +4,24 @@ from sanic import response
 from sanic.response import json
 from sanic.views import HTTPMethodView
 from sanic_cors import CORS, cross_origin
+from sanic_token_auth import SanicTokenAuth
 
 app = Sanic(__name__)
-CORS(app)
+key = open("key", "r")
+auth = SanicTokenAuth(app, secret_key=key.read().strip(),
+                      header="X-Brave-Traveler-Auth-Token")
+# CORS(app)
 
 
-class BraveTraveler(HTTPMethodView):
-    """Monitoring service for incoming data"""
-    async def get(self, request):
-        return 202
-    async def post(self, request):
-        Prepare(request.json)
-        return response.json(
-            {'message': 'Gotti'},
-            headers={'X-Served-By': 'sanic'},
-            status=200
-        )
+@app.post("/api/bravetraveler")
+@auth.auth_required
+async def bravetraveler(request):
+    Prepare(request.json)
+    return response.json(
+        {'message': 'gotti'},
+        headers={'x-served-by': 'bravetraveler'},
+        status=200
+    )
 
-app.add_route(BraveTraveler.as_view(), "/api/bravetraveler")
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True, access_log=False, workers=5)
