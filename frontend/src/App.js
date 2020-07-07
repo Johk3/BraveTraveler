185cd0e51fabcb8100cf2d44bcc894efd6643132
Johannes Leppäkorpi <user3610@protonmail.com>
2020-07-07 18:54:16 +0300

    Integrated graphs, and audio interception

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,49 @@
 import React from 'react';
 import logo from './logo.svg';
+import CanvasJSReact from './canvasjs.react'
 import './App.css';
 
+var CanvasJSChart = CanvasJSReact.CanvasJSChart;
+
+
 class App extends React.Component {
-  // constructor() {
-  //   super();
-  // }
+  constructor() {
+    super();
+    this.generateDataPoints = this.generateDataPoints.bind(this);
+  }
+
+  generateDataPoints(noOfDps) {
+    var xVal = 1, yVal = 100;
+    var dps = [];
+    for(var i = 0; i < noOfDps; i++) {
+      yVal = yVal +  Math.round(5 + Math.random() *(-5-5));
+      dps.push({x: xVal,y: yVal});	
+      xVal++;
+    }
+    return dps;
+  }
 
   render() {
+    const options = {
+      theme: "dark1", // "light1", "dark1", "dark2"
+      animationEnabled: true,
+      zoomEnabled: true,
+      title: {
+        text: "Audio Interception",
+      },
+      axisY: {
+        includeZero: false,
+        title: "Decibels",
+      },
+      axisX: {
+        title: "Iterations",
+      },
+      data: [{
+        type: "area",
+        dataPoints: this.generateDataPoints(500),
+      }]
+    }
+
     return(
     <div className="App">
       <header className="App-header">
@@ -15,6 +51,7 @@ class App extends React.Component {
         <h1>
           BraveTraveler - Monitoring
         </h1>
+        <CanvasJSChart options={options}/>
       </header>
     </div>
     )
