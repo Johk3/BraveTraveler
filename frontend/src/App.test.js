a2b456d8bda0ce690a40f54b28381c808844b13e
Johannes Leppäkorpi <user3610@protonmail.com>
2020-07-21 18:58:01 +0300

    TDD Integrated successfully

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,9 +1,21 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+// import { render } from '@testing-library/react';
+import { shallow } from 'enzyme';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/JastonMatter/i);
-  expect(linkElement).toBeInTheDocument();
+describe('MyComponent', () => {
+  it('should render correctly in "debug" mode', () => {    const component = shallow(<App debug />);
+  
+    expect(component).toMatchSnapshot();
+  });
+  it('should render correctly with no props', () => {
+    const component = shallow(<App/>);
+
+    expect(component.find("h1").text()).toContain("BraveTraveler - Monitoring")
+  });
 });
+// test('renders learn react link', () => {
+//   const { getByText } = render(<App />);
+//   const linkElement = getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
