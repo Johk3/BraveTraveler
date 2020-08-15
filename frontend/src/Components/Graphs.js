63342dc8b2a8c02cf01380ebe6549d36d8b151f6
Johannes Leppäkorpi <user3610@protonmail.com>
2020-08-15 13:03:48 +0300

    Minor

--- frontend/src/Components/Graphs.js
+++ frontend/src/Components/Graphs.js
@@ -80,7 +80,7 @@ class Graphs extends React.Component {
   }
 
   fetchData() {
-    fetch("https://api.chucknorris.io/jokes/random")
+    fetch("https://www.random.org/integers/?num=10&min=1&max=6&col=1&base=10&format=plain&rnd=new")
       .then(res => {
         return res.json()
       })
